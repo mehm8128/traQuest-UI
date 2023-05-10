a327757756cae21de3a96f3b83cbf74137c31201
mehm8128 <mehm8128@gmail.com>
2023-05-11 00:14:42 +0900

    ファイルの移動

--- src/app/_components/QuestPanel.tsx
+++ src/app/_components/QuestPanel.tsx
@@ -1,5 +1,5 @@
 import { Quest } from "@/apis/quests/types"
-import QuestTag from "@/app/components/QuestTag"
+import QuestTag from "@/app/_components/QuestTag"
 
 interface Props {
 	quest: Quest
@@ -7,7 +7,7 @@ interface Props {
 
 export default function QuestPanel(props: Props) {
 	return (
-		<div className="bg-blue-50 border border-blue-200 rounded-lg shadow-md w-80 h-56 px-4 pt-2 pb-4 flex flex-col">
+		<div className="bg-blue-50 border border-blue-200 rounded-lg shadow-md w-80 h-56 px-4 pt-2 pb-4 flex flex-col transition-all hover:shadow-lg hover:opacity-90">
 			<div className="text-xl font-bold">{props.quest.title}</div>
 			<p className="text-sm mt-2 flex-grow">{props.quest.description}</p>
 			<div className="mt-2">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import { quests } from "@/apis/quests/mocks"
-import QuestPanel from "@/app/components/QuestPanel"
+import QuestPanel from "@/app/_components/QuestPanel"
+import Link from "next/link"
 
 export default function Home() {
 	return (
 		<div>
 			<h1 className="text-3xl">traQuest</h1>
-			<div className='px-4 py-2 flex flex-wrap gap-4'>
+			<div className="px-4 py-2 flex flex-wrap gap-4">
 				{quests.map((quest) => (
-					<QuestPanel quest={quest} key={quest.id} />
+					<Link href={`/${quest.id}`} key={quest.id}>
+						<QuestPanel quest={quest} />
+					</Link>
 				))}
 			</div>
 		</div>
