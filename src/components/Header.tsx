a98f4c34199a239fd7819991ad9813fae7ea4f8a
mehm8128 <mehm8128@gmail.com>
2023-05-11 14:24:04 +0900

    ヘッダー微調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
 import UserIcon from "@/components/UserIcon"
+import Link from "next/link"
 
 export default function Header() {
 	return (
-		<header className="flex items-center justify-between bg-blue-200 py-1 px-2">
-			<h1 className="text-3xl">traQuest</h1>
+		<header className="flex items-center justify-between bg-blue-200 py-2 px-4">
+			<h1 className="text-3xl">
+				<Link href="/">traQuest</Link>
+			</h1>
 			<div className="flex items-center gap-2">
 				<div>mehm8128</div>
 				<UserIcon user="mehm8128" />
