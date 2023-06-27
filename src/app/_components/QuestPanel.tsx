a37620cffa2b448c5247cf253de54ef459c606c8
mehm8128 <mehm8128@gmail.com>
2023-06-27 23:48:46 +0900

    済を透過

--- src/app/_components/QuestPanel.tsx
+++ src/app/_components/QuestPanel.tsx
@@ -10,7 +10,7 @@ export default function QuestPanel(props: Props) {
 	return (
 		<div className="bg-blue-50 border border-blue-200 rounded-lg relative shadow-md w-[300px] h-56 px-4 pt-2 pb-4 flex flex-col gap-2 transition-all hover:shadow-lg hover:opacity-90">
 			{props.quest.completed && (
-				<div className="absolute z-0 text-center translate-x-1/2 translate-y-1/2 mx-auto text-9xl text-red-100">
+				<div className="absolute z-0 text-center translate-x-1/2 translate-y-1/2 mx-auto text-9xl text-red-100 opacity-50">
 					済
 				</div>
 			)}
