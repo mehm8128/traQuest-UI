c5eebbe5a7226b12c6c50f04e8c53995170adbf0
mehm8128 <mehm8128@gmail.com>
2023-12-12 19:59:35 +0900

    authバグってた

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,7 +2,7 @@ import { NextRequest } from "next/server"
 
 const SHOWCASE_USER_KEY = "X-Forwarded-User"
 export const getShowcaseUser = (req: NextRequest) => {
-	return "mehm8128"
+	//return "mehm8128"
 	const userid = req.headers.get(SHOWCASE_USER_KEY)
 	if (!userid) {
 		return null
